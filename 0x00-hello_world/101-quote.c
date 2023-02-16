e4e43aa9cde84098da621fb06ba7d21e78eaca4a
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-16 14:01:58 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,8 @@
 
 int main(void)
 {
-    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(msg) - 1;  // exclude null terminator
-
-    write(STDERR_FILENO, msg, len);
+    fprintf(stderr, "\"That piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return 1;
 
     return 1;
 }
