b95742e05b866d1f7e6ed4431a86bb94672d28af
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-16 13:59:24 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 
-/**
- * main - Prints quote to the standard error
- *
- * Return: zero on success
- *
- */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = sizeof(msg) - 1;  // exclude null terminator
+
+    write(STDERR_FILENO, msg, len);
+
+    return 1;
 }
