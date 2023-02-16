c22606f1b78a1970ee198437cc9190efd95a673c
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-16 13:57:07 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - Prints quote to the standard error
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
