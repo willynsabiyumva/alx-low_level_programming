70384977610feec0d133b144c1b8dd013eaf78fe
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-16 14:03:49 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,17 @@
-#include <unistd.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
-    fprintf(stderr, "\"That piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-    return 1;
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
