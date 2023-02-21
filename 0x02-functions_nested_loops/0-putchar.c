b4cba3093117f761a51571848a5ba95cf0c4123b
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-21 13:10:27 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,16 @@
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
-int main(void)
-{
 
-	_putchar('_putchar \n');
-
-	return (0);
+int main() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
 }
