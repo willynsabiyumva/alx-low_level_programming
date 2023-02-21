b6c8309aad2cf2b281ba36ec1fbc44695b7e50ec
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-21 13:23:18 -0800

    p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - Print "alx" followed by a new line.
+ * main - Print "_putchar" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
+int main(void)
+{
+	char put[] = "_putchar";
+	int i = 0;
 
-int main() {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+	while (put[i] != '\0')
+	{
+		_putchar(put[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
