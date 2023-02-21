7f310951432bb87c7a38c14fd47e91994c757f2b
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-21 13:06:12 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,8 @@
  */
 int main(void)
 {
-	char main[] = "alx";
-	int i = 0;
 
-	while (main[i] != '\0')
-	{
-		_putchar(main[i]);
-		i++;
-	}
-	_putchar('\n');
+	_putchar('_putchar \n');
 
 	return (0);
 }
