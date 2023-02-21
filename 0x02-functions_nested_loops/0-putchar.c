a4cc2ba12b739e18897fab1f700205bafe4d6f6e
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-21 08:52:52 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "alx.h"
+/**
+ * main - Print "alx" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char School[] = "alx";
+	int i = 0;
+
+	while (School[i] != '\0')
+	{
+		_putchar(School[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
