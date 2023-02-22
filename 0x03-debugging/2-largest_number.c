709b122aa26b5ad1faaf78115b0764980bc1754a
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-02-22 08:26:17 -0800

    large

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,31 +9,20 @@
  */
 
 int largest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
 {
-	int largest;
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-	if (a > b)
-	{
-		if (a > c)
-			largest = a;
-		else
-			largest = c;
-	}
-	else if (b > a)
-	{
-		if (b > c)
-			largest = b;
-		else
-			largest = c;
-	}
-	else if (a > c)
-	{
-		largest = a;
-	}
-	else
-	{
-		largest = c;
-	}
-
-	return (largest);
+return (largest);
 }
