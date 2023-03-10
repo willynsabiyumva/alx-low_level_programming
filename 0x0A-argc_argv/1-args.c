f5d20e745c8c3d9c506c8c5cfd8af92865413d2b
willynsabiyumva <willynsabiyumva27@gmail.com>
2023-03-10 11:04:02 -0800

    add

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,31 +2,41 @@
 
 
 /**
- * _puts - print a string, followed by a new line
- * @str: the string to print
+ * print_number - print an integer
+ * @n: the integer to print
  *
  * Return: void
  */
-void _puts(char *str)
+void print_number(int n)
 {
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
+	if (n > -1)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
 }
 
 
 /**
- * main - print the program name and arguments
+ * main - print the number of arguments received
  * @argc: size of the argument vector
  * @argv: program name and arguments
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	do {
-		_puts(*argv++);
-	} while (--argc > 0);
+	print_number(argc - 1);
+	_putchar('\n');
 
 	return (0);
 }
+
